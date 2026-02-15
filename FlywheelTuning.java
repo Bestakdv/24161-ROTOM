8cbed513ef108d860183895dc8bc73ff60fb42ac
Alexander Yefimenko <86662969+Bestakdv@users.noreply.github.com>
2026-02-14 21:57:45 -0600

    Update flywheel tuning parameters and logic

--- FlywheelTuning.java
+++ FlywheelTuning.java
@@ -5,52 +5,70 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
 
-@TeleOp
 @Configurable
+@TeleOp(name = "Flywheel Tuning")
 public class FlywheelTuning extends OpMode {
 
     private DcMotorEx flywheel;
-    public static double highVelocity = 1900;
-    public static double lowVelocity = 1300;
+    private VoltageSensor voltageSensor;
+
+    // TARGETS
+    public static double highVelocity = 2300;
+    public static double lowVelocity = 1800;
+
+
+    public static double F = 14.25;
+    public static double P = 0;
+
+    private static final double REFERENCE_VOLTAGE = 12.0;
+
     double curTargetVelocity = highVelocity;
-    public static double F=0;
-    public static double P=0;
+    boolean lastB = false;
 
     @Override
-    public void init()
-    {
+    public void init() {
         flywheel = hardwareMap.get(DcMotorEx.class, "flywheel");
-        flywheel.setDirection(DcMotorEx.Direction.REVERSE);
-        PIDFCoefficients PIDF = new PIDFCoefficients(P,0,0,F);
-        flywheel.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, PIDF);
-        telemetry.addLine("Init Complete");
+        voltageSensor = hardwareMap.voltageSensor.iterator().next();
+
+        flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        flywheel.setDirection(DcMotorEx.Direction.FORWARD);
+        
+        flywheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     @Override
-    public void loop(){
-
-        if(curTargetVelocity==highVelocity && gamepad1.b){
-            curTargetVelocity=lowVelocity;
+    public void loop() {
+        if (gamepad1.b && !lastB) {
+            curTargetVelocity = (curTargetVelocity == highVelocity) ? lowVelocity : highVelocity;
         }
-        if(curTargetVelocity==lowVelocity && gamepad1.b){
-            curTargetVelocity=highVelocity;
+        lastB = gamepad1.b;
+
+        double currentBatteryVoltage = voltageSensor.getVoltage();
+        double voltageMultiplier = REFERENCE_VOLTAGE / currentBatteryVoltage;
+
+        if (currentBatteryVoltage < 5) {
+            voltageMultiplier = 1.0;
         }
 
-        PIDFCoefficients PIDF = new PIDFCoefficients(P,0,0,F);
-        flywheel.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, PIDF);
+        double adjustedF = F * voltageMultiplier;
+
+        PIDFCoefficients newPIDF = new PIDFCoefficients(P, 0, 0, adjustedF);
+        flywheel.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, newPIDF);
 
         flywheel.setVelocity(curTargetVelocity);
-        double curVelocity = flywheel.getVelocity();
-        double error = curTargetVelocity - curVelocity;
-
+        
         telemetry.addData("Target Velocity", curTargetVelocity);
-        telemetry.addData("Current Velocity", curVelocity);
-        telemetry.addData("Error","%.2f", error);
-        telemetry.addLine("--------------------------------");
-        telemetry.addData("Tuning P", "%.4f",P);
-        telemetry.addData("Tuning F","%.4f", F);
-
+        telemetry.addData("Actual Velocity", flywheel.getVelocity());
+        telemetry.addLine("-----------------");
+        telemetry.addData("Battery Voltage", "%.2f V", currentBatteryVoltage);
+        telemetry.addData("Multiplier", "%.2f", voltageMultiplier);
+        telemetry.addData("Tuned F", F);
+        telemetry.addData("Effective F", "%.2f", adjustedF);
+        telemetry.update();
     }
 }
