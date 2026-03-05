ff07f61b74b4abbbfd74bc9d5d119eda5c0337bb
Alexander Yefimenko <86662969+Bestakdv@users.noreply.github.com>
2026-03-05 10:15:31 -0600

    Added pedropathing to shoot while moving

--- NewBotTeleopBlue.java
+++ NewBotTeleopBlue.java
@@ -1,168 +1,144 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.math.MathFunctions;
-import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.pedropathing.math.Vector;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.PIDFCoefficients;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.pedropathing.follower.Follower;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
+@Configurable
 @TeleOp
-public class NewBotTeleopBlue extends LinearOpMode {
+public class NewBotTeleopBlue extends OpMode {
     private Follower follower;
+    public double targetX = 6.5;
+    public double targetY = 138.0;
+    private boolean autoAimEngaged = false;
+    private boolean lastRightTrigger = false;
+    private boolean manual = false;
+    private boolean lastDpadLeft = false;
+    private VoltageSensor batteryVoltageSensor;
     private DcMotorEx curry;
     private DcMotor coreHex;
-    private DcMotor frontLeft, frontRight, backLeft, backRight;
     private DcMotor intake;
-    private Servo green;
+    public static double velocity = 700;
+    private static final int bankVelocity = 1900;
+    private static final int farVelocity = 2400;
 
-    private static final int bankVelocity = 1300;
-    private static final int farVelocity = 1900;
-    private static final double closeHood = 1;
-    private static final double farHood = 0.75;
-    private static double servo_position = 1; 
-    public static Pose startingPose; 
-    private GoBildaPinpointDriver pinpoint;
-
-    private boolean manual = false;
-    private boolean lastDpadLeft = false; 
-    private boolean lastDpadUp = false;
-    private boolean lastDpadDown = false;
+    //TODO TUNE THIS FOR YOUR FLYWHEEL MAXIMUM VELOCITY
+    private static final double MAX_MOTOR_VELOCITY = 2040.0;
+    //TODO TUNE THIS FOR LEFT STRAFE AND RIGHT FOR STRAFING RIGHT
+    private static final double PROJECTILE_SPEED_LEFT = 150.0; 
+    private static final double PROJECTILE_SPEED_RIGHT = 50.0;
 
     @Override
-    public void runOpMode() {
-        // Hardware Mapping
+    public void init() {
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(new Pose(66.075, 7.017, Math.toRadians(90)));
+
         curry = hardwareMap.get(DcMotorEx.class, "flywheel");
         coreHex = hardwareMap.get(DcMotor.class, "coreHex");
-        frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        backLeft   = hardwareMap.get(DcMotor.class, "backLeft");
-        backRight  = hardwareMap.get(DcMotor.class, "backRight");
         intake  = hardwareMap.get(DcMotor.class, "intake");
-        green = hardwareMap.get(Servo.class, "hood");
-        pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
+        batteryVoltageSensor = hardwareMap.voltageSensor.iterator().next();
 
-        MOTOR_SETTINGS();
-        waitForStart();
-        
-        follower.startTeleopDrive();
-
-        if (opModeIsActive()) {
-            while (opModeIsActive()) {
-                follower.update(); 
-                
-                mecanumDrive();
-                setFlywheelVelocity();
-                manualCoreHexAndServoControl();
-                
-                // Telemetry
-                telemetry.addData("Mode", manual ? "MANUAL" : "AUTO-AIM");
-                telemetry.addData("Flywheel Velocity", curry.getVelocity());
-                telemetry.addData("Target Dist", getDistanceToGoal());
-                telemetry.addData("Hood Pos", green.getPosition());
-                
-                telemetry.addData("X", follower.getPose().getX());
-                telemetry.addData("Y", follower.getPose().getY());
-                telemetry.addData("Heading", Math.toDegrees(follower.getPose().getHeading()));
-                telemetry.update();
-            }
-        }
-    }
-
-    private void MOTOR_SETTINGS() {
-        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        frontLeft.setDirection(DcMotor.Direction.FORWARD);
-        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        frontRight.setDirection(DcMotor.Direction.REVERSE);
-        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        backLeft.setDirection(DcMotor.Direction.FORWARD);
-        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        backRight.setDirection(DcMotor.Direction.REVERSE);
-        
-        curry.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        curry.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        curry.setDirection(DcMotor.Direction.REVERSE);
-        
+        curry.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        curry.setDirection(DcMotor.Direction.FORWARD);
         coreHex.setDirection(DcMotor.Direction.REVERSE);
         intake.setDirection(DcMotorSimple.Direction.FORWARD);
-        
-        follower = Constants.createFollower(hardwareMap);
-        follower.setStartingPose(startingPose == null ? new Pose() : startingPose);
-        
-        PIDFCoefficients PIDF = new PIDFCoefficients(1,0,0,16);
-        curry.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, PIDF);
     }
 
-    private void mecanumDrive() {
-        double FB = -gamepad1.left_stick_y; 
-        double Strafe = gamepad1.left_stick_x;
-        double Turn = -gamepad1.right_stick_x;
+    @Override
+    public void start() {
+        follower.startTeleopDrive(true);
+        follower.setMaxPower(1);
+    }
 
-        double frontLeftPower = (FB + Strafe + Turn);
-        double backLeftPower = (FB - Strafe + Turn);
-        double frontRightPower = (FB - Strafe - Turn);
-        double backRightPower = (FB + Strafe - Turn);
-
-        double[] powers = { Math.abs(frontLeftPower), Math.abs(frontRightPower), Math.abs(backLeftPower), Math.abs(backRightPower) };
-        double max = 0;
-        for (double p : powers) if (p > max) max = p;
-
-        if (max > 1.0) {
-            frontLeftPower /= max;
-            frontRightPower /= max;
-            backLeftPower /= max;
-            backRightPower /= max;
+    @Override
+    public void loop() {
+        boolean currentRightTrigger = gamepad1.right_trigger > 0.1;
+        if (currentRightTrigger && !lastRightTrigger) {
+            autoAimEngaged = !autoAimEngaged;
         }
+        lastRightTrigger = currentRightTrigger;
 
-        frontLeft.setPower(frontLeftPower);
-        backLeft.setPower(backLeftPower);
-        frontRight.setPower(frontRightPower);
-        backRight.setPower(backRightPower);
+        follower.update();
+
+        Vector currentVelocity = follower.getVelocity();
+        double vx = currentVelocity.getXComponent();
+        double vy = currentVelocity.getYComponent();
+
+        double activeProjectileSpeed = 50.0;
+        if (vy > 5.0) {
+            activeProjectileSpeed = PROJECTILE_SPEED_LEFT;
+        } else if (vy < -5.0) {
+            activeProjectileSpeed = PROJECTILE_SPEED_RIGHT;
+        } else {
+            activeProjectileSpeed = (PROJECTILE_SPEED_LEFT + PROJECTILE_SPEED_RIGHT) / 2.0;
+        }
+        double distance = Math.hypot(targetX - follower.getPose().getX(), targetY - follower.getPose().getY());
+        double timeOfFlight = distance / activeProjectileSpeed;
+
+        double vTargetX = targetX - (vx * timeOfFlight);
+        double vTargetY = targetY - (vy * timeOfFlight);
+
+        double dx = vTargetX - follower.getPose().getX();
+        double dy = vTargetY - follower.getPose().getY();
+
+        double targetHeading = Math.atan2(dy, dx);
+        double headingerror = Math.toDegrees(targetHeading) - Math.toDegrees(follower.getPose().getHeading());
+
+        if (autoAimEngaged) {
+            follower.setTeleOpDrive(
+                    -gamepad1.left_stick_y,
+                    -gamepad1.left_stick_x,
+                    headingerror / 100.0,
+                    true
+            );
+        } else {
+            follower.setTeleOpDrive(
+                    -gamepad1.left_stick_y,
+                    -gamepad1.left_stick_x,
+                    -gamepad1.right_stick_x,
+                    true
+            );
+        }
+        manualCoreHexAndServoControl();
+        setFlywheelVelocity();
+
+        telemetry.addData("Aim Mode", autoAimEngaged ? "AUTO LOCK" : "MANUAL");
+        telemetry.addData("Flywheel Mode", manual ? "MANUAL" : "AUTO-SPEED");
+        telemetry.addData("Target Heading Error Blue:", headingerror);
+        telemetry.addData("Flywheel Velocity", curry.getVelocity());
+        telemetry.addData("Distance", getDistanceToGoal());
+        telemetry.update();
     }
 
     private void manualCoreHexAndServoControl() {
-        // Cross = Forward, Triangle = Reverse
         if (gamepad1.cross) {
-            coreHex.setPower(0.7);
-        } else if (gamepad1.triangle) { 
             coreHex.setPower(-0.7);
+        } else if (gamepad1.triangle) {
+            coreHex.setPower(0.9);
         } else {
             coreHex.setPower(0);
         }
 
-        // Intake
-        if(gamepad1.left_bumper) {
-            intake.setPower(0.75);
-        } else if(gamepad1.right_bumper){
-            intake.setPower(-0.4);
+        if (gamepad1.left_bumper) {
+            intake.setPower(0.9);
+        } else if (gamepad1.right_bumper) {
+            intake.setPower(-0.9);
         } else {
             intake.setPower(0);
         }
-
-        // Hood Servo
-        boolean currentDpadUp = gamepad1.dpad_up;
-        boolean currentDpadDown = gamepad1.dpad_down;
-
-        if (currentDpadUp && !lastDpadUp) {
-            if (servo_position < 1) servo_position += 0.05;
-        }
-        if (currentDpadDown && !lastDpadDown) {
-            if (servo_position > 0) servo_position -= 0.05;
-        }
-        
-        lastDpadUp = currentDpadUp;
-        lastDpadDown = currentDpadDown;
     }
 
     private void setFlywheelVelocity() {
-        // Manual Mode Toggle
         boolean currentDpadLeft = gamepad1.dpad_left;
         if (currentDpadLeft && !lastDpadLeft) {
             manual = !manual;
@@ -171,42 +147,53 @@ public class NewBotTeleopBlue extends LinearOpMode {
 
         if (gamepad1.options) {
             curry.setPower(-0.5);
-        } 
-        else if (gamepad1.circle) {
-             if(manual){
-                curry.setVelocity(farVelocity);
-                servo_position = farHood;
-             } else {
-                double dist = getDistanceToGoal();
-                curry.setVelocity(flywheelSpeed(dist));
-                servo_position = hoodAngle(dist);
-             }
-        } 
-        else if (gamepad1.square) { 
-            curry.setVelocity(bankVelocity);
-            servo_position = closeHood;
-        } 
-        else {
-            curry.setVelocity(0);
         }
-        green.setPosition(servo_position);
+        else if (gamepad1.circle) {
+            if (manual) {
+                setFlywheelVelocityCustom(farVelocity);
+            } else {
+                double dist = getDistanceToGoal();
+                setFlywheelVelocityCustom(flywheelSpeed(dist));
+            }
+        }
+        else if (gamepad1.square) {
+            if (manual) {
+                setFlywheelVelocityCustom(bankVelocity);
+            } else {
+                double dist = getDistanceToGoal();
+                setFlywheelVelocityCustom(flywheelSpeed(dist));
+            }
+        }
+        else {
+            setFlywheelVelocityCustom(0);
+        }
     }
 
     private double getDistanceToGoal() {
         Pose p = follower.getPose();
-        return Math.hypot(6.5 - p.getX(), 138 - p.getY());
+        return Math.hypot(targetX - p.getX(), targetY - p.getY());
     }
 
     public static double flywheelSpeed(double x) {
-        //TODO CHANGE
         return MathFunctions.clamp(
-                -0.0847688 * x * x + 15.36083 * x + 1225.70392,
-                0, 2200
+                0.0000223981 * x * x * x * x - 0.0097206 * x * x * x + 1.52224 * x * x - 93.96192 * x + 3493.02631,
+                0, 2400
         );
     }
 
-    public static double hoodAngle(double x) {
-        //TODO CHANGE
-        return 0.5; 
+    private void setFlywheelVelocityCustom(double targetVelocity) {
+        if (targetVelocity == 0) {
+            curry.setPower(0);
+            return;
+        }
+
+        double currentVoltage = batteryVoltageSensor.getVoltage();
+        if (currentVoltage <= 0) {
+            currentVoltage = 12.0;
+        }
+        double basePower = targetVelocity / MAX_MOTOR_VELOCITY;
+        double compensatedPower = basePower * (12.0 / currentVoltage);
+
+        curry.setPower(Math.min(compensatedPower, 1.0));
     }
 }
